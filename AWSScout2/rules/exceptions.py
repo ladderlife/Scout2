c86cfcb8690d54712186ea3fb3762e2882b3e7cd
Jack Dubie <jdubie@users.noreply.github.com>
2017-06-21 16:32:03 -0700

    Changes to exception file

--- AWSScout2/rules/exceptions.py
+++ AWSScout2/rules/exceptions.py
@@ -3,6 +3,7 @@
 Exceptions handling
 """
 
+import json
 
 
 def process_exceptions(aws_config, exceptions_filename = None):
@@ -20,12 +21,21 @@ def process_exceptions(aws_config, exceptions_filename = None):
     with open(exceptions_filename, 'rt') as f:
         exceptions = json.load(f)
 
+        target = open('foo2.json', 'w')
+        target.write(json.dumps(aws_config))
+        target.close()
+
+        key = 'findings' # used to be 'violations'
+
     # Process exceptions
         for service in exceptions['services']:
             for rule in exceptions['services'][service]['exceptions']:
                 filtered_items = []
-                for item in aws_config['services'][service]['violations'][rule]['items']:
-                    if item not in exceptions['services'][service]['exceptions'][rule]:
+                for item in aws_config['services'][service][key][rule]['items']:
+                    if item not in [
+                        item if isinstance(item, basestring) else item['id']
+                        for item in exceptions['services'][service]['exceptions'][rule]
+                    ]:
                         filtered_items.append(item)
-                aws_config['services'][service]['violations'][rule]['items'] = filtered_items
-                aws_config['services'][service]['violations'][rule]['flagged_items'] = len(aws_config['services'][service]['violations'][rule]['items'])
+                aws_config['services'][service][key][rule]['items'] = filtered_items
+                aws_config['services'][service][key][rule]['flagged_items'] = len(aws_config['services'][service][key][rule]['items'])
